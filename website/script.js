93bc054e36e92b6601740cfd49dab4ed4e8183f2
ElvinsonW <elvinsonwijaya14@gmail.com>
2024-05-10 13:14:10 +0700

    init

--- website/script.js
+++ website/script.js
@@ -14,19 +14,19 @@ function changeImage(filename, elemenKe){
     kecil.classList.add('clicked');
 
     if(elemenKe == 1){
-        document.getElementById("desc").innerHTML="The Fire class dragon's egg is a captivating sight, adorned with swirling patterns resembling flickering flames. Its shell, though tough, emits a gentle warmth that beckons curiosity. With a light touch, one can feel the soothing heat beneath its surface. In darkness, it glows softly, offering a comforting presence. Those who approach feel a sense of reverence, as if in the presence of a powerful force of nature waiting to be unleashed.";
+        document.getElementById("desc").innerHTML="The Fire Class Dragon is a formidable force of nature, embodying the relentless power of flames. With a mere exhale, it can unleash torrents of scorching fire, turning all in its path to ash. Its very presence raises the temperature, igniting the air around it and creating an inferno that consumes all in its vicinity. Masters of combustion, these dragons can manipulate flames at will, shaping them into devastating firestorms or precise blasts of searing heat.";
     }
     else if(elemenKe == 2){
-        document.getElementById("desc").innerHTML="Imbued with the power of swirling winds and untamed air currents, the Wind Class Dragon Egg is a marvel to behold. Its shell, adorned with delicate patterns reminiscent of whirling gusts, hints at the elemental force within. This egg almost weightless feel. If you were to hold it in your palm, you might sense a gentle vibration, it also could emit a protective aura, shielding it from harm or detection by those who would seek to harm or steal it.";
+        document.getElementById("desc").innerHTML="The Wind Class Dragon emerges as a majestic embodiment of ethereal power. Its form, an elegant symphony of sinuous grace, dances upon the winds with effortless fluidity. With wings that span vast expanses, it commands the very essence of air, shaping gusts into tempests or gentle zephyrs with a mere flick of its tail. Eyes gleaming with the clarity of the skies, it navigates the heavens with unmatched agility, harnessing the boundless energy of the atmosphere.";
     }
     else if(elemenKe == 3){
-        document.getElementById("desc").innerHTML="The water class dragon egg could be characterized by its shimmering, aqua-blue shell, which is hard and smooth to the touch. It might have a pattern resembling flowing water or waves, signifying its affinity with aquatic environments.  This egg could regulate its internal temperature, ensuring the optimal conditions for the development of the embryo regardless of external environmental factors.";
+        document.getElementById("desc").innerHTML="The Water Class Dragon, a formidable entity whose mastery over water is unparalleled. With a mere thought, it can manipulate rivers, lakes, and seas alike, bending them to its will. This dragon is the embodiment of fluidity and adaptability, able to shape water into powerful torrents, swirling vortexes, or tranquil pools. Its control over the element extends to the very molecules, allowing it to freeze water into solid barriers or evaporate it into mist. ";
     }
     else if(elemenKe == 4){
-        document.getElementById("desc").innerHTML=" The Earth Class Dragon Egg exudes an aura of solidity and endurance, reflecting the unwavering resilience of its future inhabitant.  Its bears unique characteristics that set it apart from all others. Its shell, composed of layers deposits fused over centuries, boasts an extraordinary hardness, symbolizing the enduring resilience of the earth itself. This innate strength protects the life growing within and serves as a testament to the untamed power of nature.";
+        document.getElementById("desc").innerHTML="The Earth Class Dragon embodies the very essence of stability and resilience. Born from the bedrock of ancient mountains, their connection to the earth imbues them with unparalleled defensive capabilities. Their scales are as impenetrable as fortress walls, capable of withstanding the fiercest of assaults. These dragons can call upon the very ground beneath them, shaping it into towering barriers or earthen shields, rendering themselves and their allies virtually invulnerable.";
     }
     else if(elemenKe == 5){
-        document.getElementById("desc").innerHTML="The Deviant Class Dragon’s Egg doesn’t have the same characteristics for each egg. Each egg not bound by the conventional elements that shape the world, but rather pulsates with the essence of a cosmic force so rare. They are said to possess powers that transcend the boundaries of reality itself, manipulating the very fabric of existence with a mere thought. Indeed, no one knows for certain the characteristics of this class of egg, which is what makes it exceedingly rare.";
+        document.getElementById("desc").innerHTML="The Deviant Class Dragon is a mesmerizing anomaly, diverging from elemental norms with an aura of mystery and unpredictability. Its form, sleek and iridescent, hints at ancient wisdom and futuristic innovation. Unbound by elemental constraints, it possesses abilities that defy classification—rumored to bend reality and weave illusions. Some see it as a guardian of ancient secrets, others as a herald of transformation.";
     }
 }
 
@@ -54,11 +54,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     checkbox.addEventListener("change", function () {
         if (this.checked) {
-            menu.style.opacity = 1;
-            menu.style.width = 30 + "vw"
+            menu.classList.add("active")
         } else {
-            menu.style.opacity = 0;
-            menu.style.width = 0 + "vw"
+            menu.classList.remove("active")
         }
     });
 });
